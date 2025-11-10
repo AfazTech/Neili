7d6c162396877116ae685634b8fdddd68fd89053
afaz <abolfazlmajidi100@gmail.com>
2025-11-10 14:07:50 +0330

    update Polling example

--- examples/Polling.php
+++ examples/Polling.php
@@ -6,8 +6,7 @@ use Neili\Client;
 use Neili\Settings;
 use Neili\Poller;
 
-
-$token = 'TOKEN'; // telegram api bot access token
+$token = 'TOKEN';
 
 $settings = (new Settings)
     ->setAccessToken($token)
@@ -17,18 +16,32 @@ $settings = (new Settings)
     ->setPollerMaxConcurrency(100);
 
 $client = new Client($settings);
-
 $poller = new Poller($client);
 
-$poller->onUpdate(function ($update) use ($client) {
-
-
-    if (isset($update['message']['chat']['id']) && isset($update['message']['text'])) {
-        $chatId = $update['message']['chat']['id'];
-        $text = "Echo: " . $update['message']['text'];
-        $client->sendMessage($chatId, $text);
-    }
-
+// Global update handler (optional)
+$poller->onUpdate(function ($update) {
+    // Can be used for logging or handling all updates globally
 });
+
+// Separate specific handlers
+$poller->onMessage(function ($update) use ($client) {
+    $chatId = $update['message']['chat']['id'] ?? null;
+    $text = $update['message']['text'] ?? null;
+    if ($chatId && $text) {
+        $client->sendMessage($chatId, "Echo: " . $text);
+    }
+});
+
+$poller->onEditedMessage(function ($update) use ($client) {
+    $chatId = $update['edited_message']['chat']['id'] ?? null;
+    $text = $update['edited_message']['text'] ?? null;
+    if ($chatId && $text) {
+        $client->sendMessage($chatId, "Edited: " . $text);
+    }
+});
+
+// Add more onX handlers as needed
+//$poller->onCallbackQuery(...);
+//$poller->onInlineQuery(...);
 
 $poller->start();
